0ea8dd5724fa55a6411be60737ffa57800e105e8
saarzeev <saarzeev@LB-105-28.campus.ad.bgu.ac.il>
2019-12-15 14:28:25 +0200

    Environment is really working :)

--- untitled/src/Assignment.java
+++ untitled/src/Assignment.java
@@ -1,5 +1,19 @@
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import org.hibernate.*;
+
 public class Assignment {
-    public static void main(String[] args){
-        System.out.println("nusssss");
-    }
+
+	public static void main(String[] args) {
+
+
+		//SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		Session session = HibernateUtil.currentSession();
+		
+
+	}
 }
